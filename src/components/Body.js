122cfaf2a3a8fcfb1a11e139c0d87b742f199cb6
Ashlin P J <ashlinpj403@gmail.com>
2025-03-07 23:56:30 +0530

    Add Shimmer component and update Body to fetch restaurant data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useState,useEffect } from "react";
 
 
 //Body component
 const Body=()=>{
-    const [res,setRes]=useState(resList);
+    const [res,setRes]=useState([]);
+    useEffect(()=>{
+       fetchData();
+    },[]);
+
+    const fetchData=async()=>{
+        const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
+
+        const jsonData=await data.json();
+        setRes(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        console.log(jsonData);
+    };
+
+    if(res.length===0){
+        return <h1><Shimmer/></h1>
+    }
     return (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={() => {
-                    setRes(resList.filter(r => r.info.avgRating > 4.3));
+                    setRes(res.filter(r => r.info.avgRating > 4.3));
                     
                 }}>
                 Top rated restaurants
@@ -23,6 +38,9 @@ const Body=()=>{
             </div>    
         </div>        
     )
+
+    
+    
 }
 
 export default Body;
